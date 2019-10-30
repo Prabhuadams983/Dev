487f7e7689893cbe466251e09e164d7834b7f042
Prabhuadams983 <prabhuadams983@gmail.com>
2019-10-31 01:34:19 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+    constructor(){}
+  ngOnInit(){
+  }
+}
